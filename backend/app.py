b7b4144bcacb2a7218acb2fd373214935de9a362
YonaKalfa <yona.kalfa@ortmontreuil.fr>
2025-10-21 11:29:52 +0200

    création

--- backend/app.py
+++ backend/app.py
@@ -31,3 +31,13 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, host="0.0.0.0", port=5000)
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/api/hello")
+def hello():
+    return jsonify(message="Hello from Flask API 👋")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
